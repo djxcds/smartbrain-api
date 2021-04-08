c70ae2bd90e75afa3d59273c7c6ad21a2d0bdd9f
DJXCDS <24988113+djxcds@users.noreply.github.com>
2021-04-08 21:58:43 +0800

    Add cors options

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const db = knex({
       ssl : true,
     }
   });
+var corsOptions = {
+  origin: 'https://smartbrain-djxcds.netlify.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 
 const app = express();
